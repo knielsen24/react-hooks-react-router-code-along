4cbff06bf0e1f276947fc2bc1dbdfbcfb666e98c
Kevin Nielsen <knielsen24@gmail.com>
2022-05-27 11:15:43 -0700

    commit tree 1ced56e9a99967ba74cba2a456068aac01964def

--- src/index.js
+++ src/index.js
@@ -1,16 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Home!</h1>
+		</div>
+	);
+}
+
+function About() {
+	return (
+		<div>
+			<h1>This is my about component!</h1>
+		</div>
+	);
+}
+
+function Login() {
+	return (
+		<div>
+			<h1>Login</h1>
+			<form>
+				<div>
+					<input type="text" name="username" placeholder="Username" />
+				</div>
+				<div>
+					<input type="password" name="password" placeholder="Password" />
+				</div>
+				<input type="submit" value="Submit" />
+			</form>
+		</div>
+	);
 }
 
 function App() {
-  return <Home />;
+	return (
+		<Switch>
+			<Route path="/about">
+				<About />
+			</Route>
+			<Route path="/login">
+				<Login />
+			</Route>
+			<Route exact path="/">
+				<Home />
+			</Route>
+		</Switch>
+	);
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.getElementById("root"));
